b155d2da7ff55d07f58b19f9c84760e6987041d6
Christian-Liyanto <christian.liyanto@student.umn.ac.id>
2022-07-05 12:22:50 +0700

    Create Initial Team Setup for Turnbased
    Returning both team information from playfab into client

---
+++ AzureBattleSystem/InitialTeamSetup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+using PlayFab;
+using PlayFab.Samples;
+using System.Collections.Generic;
+using PlayFab.EconomyModels;
+using PlayFab.ServerModels;
+using System.Net.Http;
+using System.Net;
+
+public static class InitialTeamSetup
+{
+    //Helper Methods
+    public static PlayFabServerInstanceAPI SetupServerAPI(dynamic args, FunctionExecutionContext<dynamic> context)
+    {
+        var apiSettings = new PlayFabApiSettings
+        {
+            TitleId = context.TitleAuthenticationContext.Id,
+            DeveloperSecretKey = Environment.GetEnvironmentVariable("PLAYFAB_DEV_SECRET_KEY", EnvironmentVariableTarget.Process)
+        };
+
+        var authContext = new PlayFabAuthenticationContext
+        {
+            EntityId = context.TitleAuthenticationContext.EntityToken
+        };
+
+        return new PlayFabServerInstanceAPI(apiSettings, authContext);
+    }
+
+    //Cloud Methods
+    [FunctionName("InitialTeamSetupAzure")]
+    public static async Task<dynamic> InitialTeamSetupAzure([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
+    {
+        //Setup serverApi (Server API to PlayFab)
+        FunctionExecutionContext<dynamic> context = JsonConvert.DeserializeObject<FunctionExecutionContext<dynamic>>(await req.ReadAsStringAsync());
+        dynamic args = context.FunctionArgument;
+        PlayFabServerInstanceAPI serverApi = SetupServerAPI(args, context);
+
+        //Request Team Information (Player and Enemy)
+        var requestTeamInformation = await serverApi.GetUserDataAsync(
+            new GetUserDataRequest { 
+                PlayFabId = context.CallerEntityProfile.Lineage.MasterPlayerAccountId, Keys = new List<string>{"CurrentPlayerTeam", "EnemyTeam"}
+            }
+        );
+
+        //Convert from json to NBmonBattleDataSave
+        List<NBMonBattleDataSave> PlayerTeam = JsonConvert.DeserializeObject<List<NBMonBattleDataSave>>(requestTeamInformation.Result.Data["CurrentPlayerTeam"].Value);
+        List<NBMonBattleDataSave> EnemyTeam = JsonConvert.DeserializeObject<List<NBMonBattleDataSave>>(requestTeamInformation.Result.Data["EnemyTeam"].Value);
+
+        return null;
+    }
+}
---
+++ AzureBattleSystem/NBMonBattleDataSave.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+
+public class NBMonBattleDataSave
+{
+    public string owner { get; set; }
+    public string nickName { get; set; }
+    public string monsterId { get; set; }
+    public string uniqueId { get; set; }
+    public int gender { get; set; }
+    public int level { get; set; }
+    public int fertility { get; set; }
+    public bool selectSkillsByHP { get; set; }
+    public List<SkillByHP> setSkillByHPBoundaries { get; set; }
+    public int Quality { get; set; }
+    public bool MutationAcquired { get; set; }
+    public int MutationType { get; set; }
+    public bool NBMonLevelUp { get; set; }
+    public int expMemoryStorage { get; set; }
+    public int currentExp { get; set; }
+    public int nextLevelExpRequired { get; set; }
+    public bool fainted { get; set; }
+    public List<StatusEffectList> statusEffectList { get; set; }
+    public List<string> skillList { get; set; }
+    public List<string> uniqueSkillList { get; set; }
+    public List<string> passiveList { get; set; }
+    public List<string> temporaryPassives { get; set; }
+    public int hp { get; set; }
+    public int energy { get; set; }
+    public int maxHp { get; set; }
+    public int maxEnergy { get; set; }
+    public int speed { get; set; }
+    public int battleSpeed { get; set; }
+    public int attack { get; set; }
+    public int specialAttack { get; set; }
+    public int defense { get; set; }
+    public int specialDefense { get; set; }
+    public int criticalHit { get; set; }
+    public float attackBuff, specialAttackBuff, defenseBuff, specialDefenseBuff, criticalBuff, ignoreDefenses, damageReduction, energyShieldValue;
+    public int energyShield { get; set; }
+    public int surviveLethalBlow { get; set; }
+    public int totalIgnoreDefense { get; set; }
+    public int mustCritical { get; set; }
+    public int immuneCritical { get; set; }
+    public int elementDamageReduction { get; set; }
+    public int maxHpPotential { get; set; }
+    public int maxEnergyPotential { get; set; }
+    public int speedPotential { get; set; }
+    public int attackPotential { get; set; }
+    public int specialAttackPotential { get; set; }
+    public int defensePotential { get; set; }
+    public int specialDefensePotential { get; set; }
+    public int maxHpEffort { get; set; }
+    public int maxEnergyEffort { get; set; }
+    public int speedEffort { get; set; }
+    public int attackEffort { get; set; }
+    public int specialAttackEffort { get; set; }
+    public int defenseEffort { get; set; }
+    public int specialDefenseEffort { get; set; }
+}
+
+public class StatusEffectList
+{
+    public int statusEffect { get; set; }
+    public int counter { get; set; }
+    public int stacks { get; set; }
+}
+
+public class SkillByHP
+{
+    public int HPHasToBeLowerThan;
+    public int HPHasToBeBiggerThan;
+    public string SkillName;
+}
