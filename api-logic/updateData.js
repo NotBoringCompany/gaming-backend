f61fd38c3440d91c83be04186d042b0064d2d879
juandogels <suwandresukijat@gmail.com>
2022-07-23 22:50:59 +0200

    refactored updateData

--- api-logic/updateData.js
+++ api-logic/updateData.js
@@ -9,46 +9,9 @@ const Moralis = require('moralis/node');
 const updateGenesisNBMonData = async (
     nbmonId,
     nickname,
-    monsterId,
-    uniqueId,
     level,
-    selectSkillsByHP,
-    setSkillByHPBoundaries,
-    Quality,
-    NBMonLevelUp,
-    expMemoryStorage,
     currentExp,
-    nextLevelExpRequired,
-    fainted,
-    statusEffectList,
     skillList,
-    uniqueSkillList,
-    temporaryPassives,
-    hp,
-    energy,
-    maxHp,
-    maxEnergy,
-    speed,
-    battleSpeed,
-    attack,
-    specialAttack,
-    defense,
-    specialDefense,
-    criticalHit,
-    attackBuff,
-    specialAttackBuff,
-    defenseBuff,
-    specialDefenseBuff,
-    criticalBuff,
-    ignoreDefenses,
-    damageReduction,
-    energyShieldValue,
-    energyShield,
-    surviveLethalBlow,
-    totalIgnoreDefense,
-    mustCritical,
-    immuneCritical,
-    elementDamageReduction,
     maxHpEffort,
     maxEnergyEffort,
     speedEffort,
@@ -79,46 +42,9 @@ const updateGenesisNBMonData = async (
         let result = await gdQuery.first({useMasterKey: true});
 
         result.set("nickname", nickname);
-        result.set("monsterId", monsterId);
-        result.set("uniqueId", uniqueId);
         result.set("level", level);
-        result.set("selectSkillsByHP", selectSkillsByHP);
-        result.set("setSkillByHPBoundaries", setSkillByHPBoundaries);
-        result.set("Quality", Quality);
-        result.set("NBMonLevelUp", NBMonLevelUp);
-        result.set("expMemoryStorage", expMemoryStorage);
         result.set("currentExp", currentExp);
-        result.set("nextLevelExpRequired", nextLevelExpRequired);
-        result.set("fainted", fainted);
-        result.set("statusEffectList", statusEffectList);
         result.set("skillList", skillList);
-        result.set("uniqueSkillList", uniqueSkillList);
-        result.set("temporaryPassives", temporaryPassives);
-        result.set("hp", hp);
-        result.set("energy", energy);
-        result.set("maxHp", maxHp);
-        result.set("maxEnergy", maxEnergy);
-        result.set("speed", speed);
-        result.set("battleSpeed", battleSpeed);
-        result.set("attack", attack);
-        result.set("specialAttack", specialAttack);
-        result.set("defense", defense);
-        result.set("specialDefense", specialDefense);
-        result.set("criticalHit", criticalHit);
-        result.set("attackBuff", attackBuff);
-        result.set("specialAttackBuff", specialAttackBuff);
-        result.set("defenseBuff", defenseBuff);
-        result.set("specialDefenseBuff", specialDefenseBuff);
-        result.set("criticalBuff", criticalBuff);
-        result.set("ignoreDefenses", ignoreDefenses);
-        result.set("damageReduction", damageReduction);
-        result.set("energyShieldValue", energyShieldValue);
-        result.set("energyShield", energyShield);
-        result.set("surviveLethalBlow", surviveLethalBlow);
-        result.set("totalIgnoreDefense", totalIgnoreDefense);
-        result.set("mustCritical", mustCritical);
-        result.set("immuneCritical", immuneCritical);
-        result.set("elementDamageReduction", elementDamageReduction);
         result.set("maxHpEffort", maxHpEffort);
         result.set("maxEnergyEffort", maxEnergyEffort);
         result.set("speedEffort", speedEffort);
