88b1a7debacb3b9b2b26c039a8602e9190ef24a5
juandogels <suwandresukijat@gmail.com>
2022-07-26 21:15:31 +0200

    implemented middleware to check for session token and ownership of nbmon id to update, commented updateData until middleware is fully tested

--- api-logic/updateData.js
+++ api-logic/updateData.js
@@ -5,66 +5,57 @@ const Moralis = require('moralis/node');
 
 /**
  * `updateGenesisNBMonData` updates the NBMon's game data and updates it to Moralis DB.
+ * @param {string} sessionId the Moralis Session ID of the logged in user
+ * @param {number} nbmonId the nbmon ID to be updated
  */
-const updateGenesisNBMonData = async (
-    nbmonId,
-    nickName,
-    level,
-    currentExp,
-    skillList,
-    maxHpEffort,
-    maxEnergyEffort,
-    speedEffort,
-    attackEffort,
-    specialAttackEffort,
-    defenseEffort,
-    specialDefenseEffort
-) => {
-    try {
-        // we are trying to query for the respective nbmon from Genesis_NBMons_GameData
-        // by querying the nbmon via `nbmonId` in the Genesis_NBMons class and then
-        // using the query result to obtain the nbmon in Genesis_NBMons_GameData
-        let GameData = Moralis.Object.extend("Genesis_NBMons_GameData");
-        let gameData = new GameData();
-        let gdQuery = new Moralis.Query(gameData);
+// const updateGenesisNBMonData = async (
+//     sessionId,
+//     nbmonId
+// ) => {
+//     try {
+//         // we are trying to query for the respective nbmon from Genesis_NBMons_GameData
+//         // by querying the nbmon via `nbmonId` in the Genesis_NBMons class and then
+//         // using the query result to obtain the nbmon in Genesis_NBMons_GameData
+//         let GameData = Moralis.Object.extend("Genesis_NBMons_GameData");
+//         let gameData = new GameData();
+//         let gdQuery = new Moralis.Query(gameData);
         
-        let GenesisNBMons = Moralis.Object.extend("Genesis_NBMons");
-        let nbmonQuery = new Moralis.Query(GenesisNBMons);
+//         let GenesisNBMons = Moralis.Object.extend("Genesis_NBMons");
+//         let nbmonQuery = new Moralis.Query(GenesisNBMons);
 
-        nbmonQuery.equalTo("NBMon_ID", nbmonId);
-        // since Genesis_NBMons_GameData contains a pointer to the actual nbmon
-        // in Genesis_NBMons, we pass the query to obtain the respective record.
-        gdQuery.matchesQuery("NBMon_Instance", nbmonQuery);
+//         nbmonQuery.equalTo("NBMon_ID", nbmonId);
+//         // since Genesis_NBMons_GameData contains a pointer to the actual nbmon
+//         // in Genesis_NBMons, we pass the query to obtain the respective record.
+//         gdQuery.matchesQuery("NBMon_Instance", nbmonQuery);
 
-        // the nbmon record is obtained and we now can update its stats
-        let result = await gdQuery.first({useMasterKey: true});
+//         // the nbmon record is obtained and we now can update its stats
+//         let result = await gdQuery.first({useMasterKey: true});
 
-        result.set("nickName", nickName);
-        result.set("level", level);
-        result.set("currentExp", currentExp);
-        result.set("skillList", skillList);
-        result.set("maxHpEffort", maxHpEffort);
-        result.set("maxEnergyEffort", maxEnergyEffort);
-        result.set("speedEffort", speedEffort);
-        result.set("attackEffort", attackEffort);
-        result.set("specialAttackEffort", specialAttackEffort);
-        result.set("defenseEffort", defenseEffort);
-        result.set("specialDefenseEffort", specialDefenseEffort);
+//         result.set("nickName", nickName);
+//         result.set("level", level);
+//         result.set("currentExp", currentExp);
+//         result.set("skillList", skillList);
+//         result.set("maxHpEffort", maxHpEffort);
+//         result.set("maxEnergyEffort", maxEnergyEffort);
+//         result.set("speedEffort", speedEffort);
+//         result.set("attackEffort", attackEffort);
+//         result.set("specialAttackEffort", specialAttackEffort);
+//         result.set("defenseEffort", defenseEffort);
+//         result.set("specialDefenseEffort", specialDefenseEffort);
 
-        let objSaved;
-        result.save(null, {useMasterKey: true}).then((obj) => {
-            objSaved = obj;
-        });
+//         let objSaved;
+//         result.save(null, {useMasterKey: true}).then((obj) => {
+//             objSaved = obj;
+//         });
 
-        return {
-            updated: "ok",
-            obj: objSaved
-        }
-    } catch (err) {
-        throw err;
-    }
-}
-
+//         return {
+//             updated: "ok",
+//             obj: objSaved
+//         }
+//     } catch (err) {
+//         throw err;
+//     }
+// }
 
 module.exports = {
     updateGenesisNBMonData
---
+++ middlewares/checkSession.js
@@ -0,0 +1,112 @@
+const Moralis = require('moralis/node');
+const ethers = require('ethers');
+const fs = require('fs');
+const path = require('path');
+
+const nodeURL = process.env.RPC_URL;
+
+const customHttpProvider = new ethers.providers.JsonRpcProvider(nodeURL);
+
+const genesisNBMonABI = fs.readFileSync(
+	path.resolve(__dirname, "../abi/genesisNBMon.json")
+);
+const genesisABI = JSON.parse(genesisNBMonABI);
+const genesisContract = new ethers.Contract(
+	process.env.CONTRACT_ADDRESS,
+	genesisABI,
+	customHttpProvider
+);
+
+/**
+ * @dev Helper function to parse object data into a JSON string
+ */
+const parseJSON = (data) => JSON.parse(JSON.stringify(data));
+
+/**
+ * 
+ * @param {string} sessionToken is the session token of the user
+ * @param {number} nbmonId is the nbmon id that needs to be updated.
+ * `updateGenesisNBMonCheck` checks if the user is logged in and the session token belongs to the user.
+ * if it does, it checks if the nbmon ID that is being updated/to be updated belongs to this user.
+ */
+const updateGenesisNBMonCheck = async (req, res, next) => {
+    try {
+        const {sessionToken, nbmonId} = req.body;
+        
+        // we get an instance of the Session DB for querying
+        const SessionDB = new Moralis.Query("_Session");
+        // we get an instance of the User DB for querying
+        const UserDB = new Moralis.Query("_User");
+
+        // we get all the data of the record that contains the session token
+        const sessionTokenQuery = SessionDB.equalTo("sessionToken", sessionToken);
+        const queryResult = await sessionTokenQuery.first({useMasterKey: true});
+
+        // if the query is undefined, the session token isn't found in the database.
+        if (queryResult === undefined) {
+            throw new Error("Session token not found. Please try again.");
+        }
+        const queryResultParsed = parseJSON(queryResult);
+        // we obtain the pointer (obj ID) to the user from the _Session class.
+        const userObjId = queryResultParsed["user"]["objectId"];
+
+        // now we query for the user using the object ID from the _Session class.
+        const userQuery = UserDB.equalTo("objectId", userObjId);
+        const userQueryResult = await userQuery.first({useMasterKey: true});
+
+        // if somehow the user can't be found, this error will be thrown.
+        if (userQueryResult === undefined) {
+            throw new Error("User not found. Please try again.");
+        }
+
+        const userQueryResultParsed = parseJSON(userQueryResult);
+
+        // we now obtain the eth address of the user
+        const ethAddress = userQueryResultParsed["ethAddress"];
+
+        // if somehow the user doesn't have an eth address, this error will be thrown.
+        if (ethAddress === undefined || ethAddress.length === 0) {
+            throw new Error("User does not have an ETH address.");
+        }
+
+        // now we obtain the IDs owned by this user from the genesis contract.
+        const ownerIds = await genesisContract.getOwnerNFTIds(ethAddress);
+
+        // if the user doesn't own any nbmons, then this error is thrown since there is no need
+        // for further checks.
+        if (ownerIds.length === 0) {
+            throw new Error("User does not own any NBMons");
+        }
+        
+        // by default, the result returned from Solidity's uints are always in BigNumber.
+        // this will convert them into numbers.
+        let convertedArray = [];
+        for (let i = 0; i < ownerIds.length; i++) {
+            let converted = parseInt(Number(ownerIds[i]));
+            convertedArray.push(converted);
+        }
+
+        // this is a boolean value that will return true if the user does own the specified `nbmonId`.
+        let isOwner = false;
+
+        // now we check if the nbmonId that is being updated is owned by the owner.
+        for (let j = 0; j < convertedArray.length; j++) {
+            if (convertedArray[j] === parseInt(nbmonId)) {
+                isOwner = true;
+            }
+        }
+
+        // if isOwner is still false, then we know the user doesn't own the nbmon. an error will be thrown.
+        if (isOwner === false) {
+            throw new Error(`${nbmonId} is not owned by the user.`);
+        } else {
+            next();
+        }
+    } catch (err) {
+        throw err;
+    }   
+}
+
+updateGenesisNBMonCheck("r:cdcd24188de039bf576bb6036c3fc0f4", 1);
+
+module.exports = { updateGenesisNBMonCheck }
