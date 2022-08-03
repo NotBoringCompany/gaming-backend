a9d0476215cda1e28746d8cf260dc9700451a635
Christian-Liyanto <christian.liyanto@student.umn.ac.id>
2022-08-03 12:02:43 +0700

    Add Crafting Function with xRes Cost

---
+++ AzurePlayerAccount/CraftingItem.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PlayFab;
+using PlayFab.Samples;
+using PlayFab.ServerModels;
+using System.Collections.Generic;
+
+
+public static class CraftingItem{
+    //Helper Methods
+    public static PlayFabServerInstanceAPI SetupServerAPI(dynamic args, FunctionExecutionContext<dynamic> context)
+    {
+        var apiSettings = new PlayFabApiSettings
+        {
+            TitleId = context.TitleAuthenticationContext.Id,
+            DeveloperSecretKey = Environment.GetEnvironmentVariable("PLAYFAB_DEV_SECRET_KEY", EnvironmentVariableTarget.Process)
+        };
+
+        var authContext = new PlayFabAuthenticationContext
+        {
+            EntityId = context.TitleAuthenticationContext.EntityToken
+        };
+
+        return new PlayFabServerInstanceAPI(apiSettings, authContext);
+    }
+
+    //Azure Function
+    [FunctionName("CraftItem")]
+    public static async Task<dynamic> CraftItem([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log){
+        //Setup serverApi (Server API to PlayFab)
+        FunctionExecutionContext<dynamic> context = JsonConvert.DeserializeObject<FunctionExecutionContext<dynamic>>(await req.ReadAsStringAsync());
+        dynamic args = context.FunctionArgument;
+        PlayFabServerInstanceAPI serverApi = SetupServerAPI(args, context);
+
+        //Declare Variable
+        List<string> itemIdList = new List<string>();
+        string itemId = "";
+        int itemQuantity;
+        double totalCost = 0f;
+
+        //Get ID Data From Client
+        if(args["itemID"] != null)
+            itemId = args["itemID"];
+
+        //Get Quantity Data From Client
+        if(args["itemQuantity"] != null)
+            itemQuantity = (int) args["itemQuantity"];
+        else
+            itemQuantity = 1;
+
+        //Add Item Into List Based On Quantity
+        for(int i = 0; i < itemQuantity; i++){
+            itemIdList.Add(itemId);
+        }
+
+        //Get total Cost From Client
+        if(args["totalCost"] != null)
+            totalCost = (double) args["totalCost"];
+
+        //Grant User The Item List
+        var reqGrantItem = await serverApi.GrantItemsToUserAsync(
+            new GrantItemsToUserRequest{
+                PlayFabId = context.CallerEntityProfile.Lineage.MasterPlayerAccountId,
+                ItemIds = itemIdList,
+                CatalogVersion = "InventoryTest"
+            }
+        );
+
+        var reqUserReadOnly = await serverApi.GetUserReadOnlyDataAsync(
+            new GetUserDataRequest{
+                PlayFabId = context.CallerEntityProfile.Lineage.MasterPlayerAccountId
+            }
+        );
+
+        //Request User Inventory Item
+        var reqUserInventory = await serverApi.GetUserInventoryAsync(
+            new GetUserInventoryRequest{
+                PlayFabId = context.CallerEntityProfile.Lineage.MasterPlayerAccountId
+            }
+        );
+
+        //Deduct xRes From User
+        double xResUser = float.Parse(reqUserReadOnly.Result.Data["xRES"].Value);
+        xResUser -= totalCost;
+        xResUser = Math.Round(xResUser, 2);
+
+        //Update xRes into PlayFab
+        var updateUserReadOnly = await serverApi.UpdateUserReadOnlyDataAsync(
+            new UpdateUserDataRequest{
+                PlayFabId = context.CallerEntityProfile.Lineage.MasterPlayerAccountId,
+                Data = new Dictionary<string, string>{
+                    {"xRES", xResUser.ToString()}
+                }
+            }
+        );
+
+        List<ItemInstance> ItemData = reqUserInventory.Result.Inventory;
+        //Let's update Player Inventory if the Player's item exceeded maximum item or not.
+        foreach (var IndividualItemInstance in ItemData)
+        {
+            if (IndividualItemInstance.RemainingUses >= 99)
+            {
+                var requestModifyItem = await serverApi.ModifyItemUsesAsync(new ModifyItemUsesRequest()
+                {
+                    PlayFabId = context.CallerEntityProfile.Lineage.MasterPlayerAccountId,
+                    ItemInstanceId = IndividualItemInstance.ItemInstanceId,
+                    UsesToAdd = (int)(99 - IndividualItemInstance.RemainingUses)
+                });
+            }
+        }
+
+        //Return Current user xRes
+        return xResUser.ToString();
+    }
+}
