42d7a5d4820e65294887b423d27108714ca4dd1a
Christian-Liyanto <christian.liyanto@student.umn.ac.id>
2022-08-16 12:02:20 +0700

    Create Item Database for Realm and Query Logic to return Data

--- TestingFolder/CosmosDbTest.cs
+++ TestingFolder/CosmosDbTest.cs
@@ -21,6 +21,11 @@ public static class CosmosDbTest
         public string Id { get; set; }
         public string Description { get; set; }
     }
+    
+    public class I
+    {
+        public List<ItemsPlayFab> item {get; set;}
+    }
 
     public class ToDoItemLookup
     {
@@ -50,39 +55,63 @@ public static class CosmosDbTest
     }
 
     [FunctionName("DocsByUsingDocumentClient")]
-        public static async Task<IActionResult> Run(
+        public static dynamic Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post",
                 Route = null)]HttpRequest req,
             [CosmosDB(
-                databaseName: "TestDb",
-                collectionName: "Data",
+                databaseName: "RealmDb",
+                collectionName: "ItemsData",
                 ConnectionStringSetting = "CosmosDBConnection")] DocumentClient client,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-            
-            List<ToDoItem> toDoItems = new List<ToDoItem>();
 
             //An option that is a must if 
             var option = new FeedOptions(){ EnableCrossPartitionQuery = true };
-            Uri collectionUri = UriFactory.CreateDocumentCollectionUri("TestDb", "Data");
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri("RealmDb", "ItemsData");
 
             //Query Data Function, turn it as a List of something you use as a Data Type.
-            List<ToDoItem> query = client.CreateDocumentQuery<ToDoItem>(collectionUri, "SELECT * FROM database db WHERE db.description = 'This is work description'",option).ToList();
-            //List<ToDoItem> query = client.CreateDocumentQuery<ToDoItem>(collectionUri, "SELECT * FROM database db",option).ToList();
+            ItemsPlayFab item = new ItemsPlayFab();
+            item = client.CreateDocumentQuery<ItemsPlayFab>(collectionUri, "SELECT * FROM db WHERE db.Name = 'Complete Recovery Kit'",option).AsEnumerable().FirstOrDefault();
 
-            // log.LogInformation(query.HasMoreResults.ToString());
-
-            // while (query.HasMoreResults)
-            // {
-            //     foreach (ToDoItem result in await query.ExecuteNextAsync())
-            //     {
-            //         toDoItems.Add(result);
-            //     }
-            // }
-
-            log.LogInformation(JsonConvert.SerializeObject(query));
+            log.LogInformation(JsonConvert.SerializeObject(item));
             
-            return new OkResult();
+            return JsonConvert.SerializeObject(item);
         }
+
+        // [FunctionName("DocsByUsingDocumentClient")]
+        // public static IActionResult Run(
+        //     [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post",
+        //         Route = null)]HttpRequest req,
+        //     [CosmosDB(
+        //         databaseName: "TestDb",
+        //         collectionName: "Data",
+        //         ConnectionStringSetting = "CosmosDBConnection")] DocumentClient client,
+        //     ILogger log)
+        // {
+        //     log.LogInformation("C# HTTP trigger function processed a request.");
+            
+        //     List<ToDoItem> toDoItems = new List<ToDoItem>();
+
+        //     var option = new FeedOptions(){ EnableCrossPartitionQuery = true };
+
+        //     Uri collectionUri = UriFactory.CreateDocumentCollectionUri("TestDb", "Data");
+
+        //     // List<ToDoItem> query = client.CreateDocumentQuery<ToDoItem>(collectionUri, "SELECT * FROM database db WHERE db.description = 'This is work description'",option).ToList();
+        //     List<ToDoItem> query = client.CreateDocumentQuery<ToDoItem>(collectionUri, "SELECT * FROM database db",option).ToList();
+
+        //     // log.LogInformation(query.HasMoreResults.ToString());
+
+        //     // while (query.HasMoreResults)
+        //     // {
+        //     //     foreach (ToDoItem result in await query.ExecuteNextAsync())
+        //     //     {
+        //     //         toDoItems.Add(result);
+        //     //     }
+        //     // }
+
+        //     log.LogInformation(JsonConvert.SerializeObject(query));
+            
+        //     return new OkResult();
+        // }
 }
